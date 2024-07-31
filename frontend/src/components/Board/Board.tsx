935f0fe48279ec59462eb56fad4eb875845e542e
LAPTOP-B29GO558\adamf <adamfinkler6@gmail.com>
2024-07-31 16:40:53 +0300

    added images of soldiers

--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -4,8 +4,8 @@ import { Row } from "./Row/Row";
 
 export const Board = () => {
   const rows = Array(8).fill(null);
-  const BoardArr:Soldier[][] = initalizeBoard();
-  
+  // const BoardArr:Soldier[][] = initalizeBoard();
+
   return (
     <div className="board-container">
       {rows.map((_, index) => (
